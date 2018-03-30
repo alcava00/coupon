53908bd23de3a80a7364d7abd764515affa0ff7d
P127260 <alcava00@sk.com>
2018-03-30 14:52:28 +0900

    Coupon생성기 id 중복 체크 test 코드 추가

--- src/test/java/com/example/coupon/service/RandomUniqueIdGeneratorTest.java
+++ src/test/java/com/example/coupon/service/RandomUniqueIdGeneratorTest.java
@@ -4,6 +4,9 @@ import com.example.coupon.config.CouponProperties;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by alcava00 on 2018. 3. 26..
  */
@@ -11,6 +14,9 @@ public class RandomUniqueIdGeneratorTest extends Assert {
     private final String regExp= "^[\\w]{4}-[\\w]{4}-[\\w]{4}-[\\w]{4}$";
 
     private RandomUniqueIdGenerator randomUniqueIdGenerator=new RandomUniqueIdGenerator(new CouponProperties());
+    /**
+     * 결과값 포맷 체크
+     * */
     @Test
     public void isValidFormat() throws Exception {
         long time=System.currentTimeMillis();
@@ -20,6 +26,24 @@ public class RandomUniqueIdGeneratorTest extends Assert {
             if (!id.matches(regExp)) {
                 assertFalse(id, true);
             }
+        }
+    }
+
+
+    /**
+     * 중복값 존재 여부 확인
+     * */
+    @Test
+    public void  checkDup() throws Exception {
+         Set<String> set=new HashSet<>();
+        for (int i = 0; i < 1000000; i++) {
+            String id = randomUniqueIdGenerator.generateUniqueId();
+            if(set.contains(id)){
+                assertFalse(id, true);
+            }else{
+                set.add(id);
+            }
+
         }
     }
 
